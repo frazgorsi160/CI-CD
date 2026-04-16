2901a5522a793ad84686acea0e07bf0bbb7369e8
Omair3651 <f223651@cfd.nu.edu.pk>
2026-04-16 11:11:27 +0500

    updated code by ci/cd

--- Example/src/main/webapp/main.java
+++ Example/src/main/webapp/main.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins");
+        System.out.println("Hello World - Updated by CI/CD");
     }
 }
